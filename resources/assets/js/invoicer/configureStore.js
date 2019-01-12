0d793d21738eb9ca537fe06f83ece0faa8f42020
SaintPeter <rex.schrader@gmail.com>
2019-01-12 15:20:22 -0800

    Add redux-logger

--- resources/assets/js/invoicer/configureStore.js
+++ resources/assets/js/invoicer/configureStore.js
@@ -1,10 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
 import rootReducer from './reducers/reducer';
 
 export const store = createStore(
   rootReducer,
-  applyMiddleware(thunk)
+  applyMiddleware(thunk),
+  applyMiddleware(logger),
 );
 
