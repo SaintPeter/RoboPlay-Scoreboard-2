518b4e3386732be790997d2ad8a849608eb0827d
SaintPeter <rex.schrader@gmail.com>
2019-02-18 14:23:58 -0800

    Video Review - DB and Models

---
+++ app/Models/Video_review_problems.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Video_review_problems extends Model
+{
+
+	public function video() {
+		return $this->belongsTo(Videos::class);
+	}
+
+	public function reviewer() {
+		return $this->belongsTo(User::class,'reviewer_id');
+	}
+
+	public function resolver() {
+		return $this->belongsTo(User::class,'resolver_id');
+	}
+
+}
