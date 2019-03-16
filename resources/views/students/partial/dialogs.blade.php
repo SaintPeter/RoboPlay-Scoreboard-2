185b87743e6cdf08e0643f3d0190324fda3ba552
SaintPeter <rex.schrader@gmail.com>
2019-03-16 13:04:54 -0700

    Hide dialogs during page load

--- resources/views/students/partial/dialogs.blade.php
+++ resources/views/students/partial/dialogs.blade.php
@@ -1,9 +1,9 @@
 
-<div id="choose_students_dialog" title="Choose Students">
+<div id="choose_students_dialog" style="display: none" title="Choose Students">
 
 </div>
 
-<div id="mass_upload_students_dialog" title="Choose Upload File">
+<div id="mass_upload_students_dialog" style="display: none" title="Choose Upload File">
 Download this <a href="/docs/roboplay_scoreboard_student_upload_template.xlsx" target="_blank">Excel Template</a> and follow the instructions inside to generate a csv file for upload.<br><br>
   {!! Form::open([ 'route' => 'ajax.import_students_csv', 'files' => true, 'id' => 'upload_form' ] )  !!}
 	  {!! Form::label('csv_file', 'CSV File', array('id'=>'','class'=>''))  !!}
