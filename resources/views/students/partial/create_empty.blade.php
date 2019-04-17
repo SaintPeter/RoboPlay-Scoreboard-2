4c4be3583a745c9711f3563f4d719cb8b765cdbb
SaintPeter <rex.schrader@gmail.com>
2019-04-17 16:54:25 -0700

    Fix variable error

--- resources/views/students/partial/create_empty.blade.php
+++ resources/views/students/partial/create_empty.blade.php
@@ -33,7 +33,7 @@
 		</div>
 		<div class="form-group">
 			<label for="students[{{ $index }}][grade]">Grade
-			{!! Form::select("students[$index][grade]", [ 0 => "-", 5 => 5, 6 => 6, 7=> 7,8=> 8, 9=> 9, 10 => 10, 11 => 11, 12 => 12, 13 => 13, 14=> 14 ], $student['grade'], [ 'class' => 'form-control' ] )  !!}</label>
+			{!! Form::select("students[$index][grade]", [ 0 => "-", 5 => 5, 6 => 6, 7=> 7,8=> 8, 9=> 9, 10 => 10, 11 => 11, 12 => 12, 13 => 13, 14=> 14 ], null, [ 'class' => 'form-control' ] )  !!}</label>
 		</div>
 		<div class="form-group">
 			<label class="sr-only" for="students[{{$index}}][email]">E-mail</label>
