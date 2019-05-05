34bea152657344628eba70d3e9b793f41a5c6ebd
SaintPeter <rex.schrader@gmail.com>
2019-05-05 16:18:14 -0700

    Fix initial default score display

--- resources/assets/js/scorer/components/ScoreElements/ScoreElement.js
+++ resources/assets/js/scorer/components/ScoreElements/ScoreElement.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 
 const chalTextStyle = {
   width: "85%",
@@ -6,48 +6,52 @@ const chalTextStyle = {
 };
 
 export default class ScoreElement extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.multi = (props.type==='score_slider') ? 1 : props.multiplier;
-        this.state = {
-            score: Math.max(this.props.min_entry, this.props.base_value),
+    this.multi = (props.type === 'score_slider') ? 1 : props.multiplier;
+    this.state = {
+      score: Math.max(this.props.min_entry, this.props.base_value),
+    }
+  }
+
+  // Calculate the individual score for this element when it changes
+  handleScoreChange = (value) => {
+    const baseScore = this.props.base_value + (value * this.multi) + (Math.pow(value, 2) * this.props.multiplier2);
+    let newScore = baseScore;
+    if (this.props.score_map.length) {
+      for (let i = this.props.score_map.length - 1; i > -1; i--) {
+        if (baseScore >= this.props.score_map[i].i) {
+          newScore = this.props.score_map[i].v * 1;
+          break;
         }
+      }
     }
 
-    // Calculate the individual score for this element when it changes
-    handleScoreChange = (value) => {
-        const baseScore = this.props.base_value + (value * this.multi) + (Math.pow(value,2) * this.props.multiplier2);
-        let newScore = baseScore;
-        if(this.props.score_map.length) {
-          for(let i = this.props.score_map.length - 1; i > -1; i--) {
-              if(baseScore >= this.props.score_map[i].i) {
-                  newScore = this.props.score_map[i].v * 1;
-                  break;
-              }
-          }
-        }
-
-        // Enforce min and max values
-        if(this.props.enforce_limits) {
-            newScore = Math.min(this.props.max_entry, Math.max(this.props.min_entry, newScore));
-        }
-
-        // Update my state
-        this.setState({score: newScore});
-
-        // Send the change up to ScoreChallenge
-        this.props.scoreChange({[this.props.element_number]: newScore });
-    };
-
-    render() {
-        return (
-            <li className="ui-field-contain ui-li-static ui-body-inherit">
-                <span className="ui-li-count score_display">{ this.state.score }</span>
-                <div style={chalTextStyle} dangerouslySetInnerHTML={{ __html: this.props.display_text }} />
-                {React.cloneElement(this.props.children, { onChange: this.handleScoreChange })}
-            </li>
-        );
+    // Enforce min and max values
+    if (this.props.enforce_limits) {
+      newScore = Math.min(this.props.max_entry, Math.max(this.props.min_entry, newScore));
     }
+
+    // This dirty hack to attempt to work around race/timing issues
+    // Forces the setState which occurs upstream to happen sequentially
+    setTimeout(() => {
+      // Update my state
+      this.setState({score: newScore});
+
+      // Send the change up to ScoreChallenge
+      this.props.scoreChange({[this.props.element_number]: newScore});
+    }, 1)
+  };
+
+  render() {
+    return (
+      <li className="ui-field-contain ui-li-static ui-body-inherit">
+        <span className="ui-li-count score_display">{this.state.score}</span>
+        <div style={chalTextStyle} dangerouslySetInnerHTML={{__html: this.props.display_text}}/>
+        {React.cloneElement(this.props.children, {onChange: this.handleScoreChange})}
+      </li>
+    );
+  }
 
 }
